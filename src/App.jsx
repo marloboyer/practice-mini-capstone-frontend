a6e3e2c5ff15b97e5b421ab51476676cd267e1d0
marloboyer <marlo.boyer7@yahoo.com>
2023-04-17 19:39:53 -0500

    Added Products New to allow someone to type in a new products name, description, price and url

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,23 @@ function Header() {
   );
 }
 
-function ProductsNew() {}
+function ProductsNew() {
+  return (
+    <div>
+      <h2>New Product</h2>
+      <form>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name"></input>
+        <label for="description">Description</label>
+        <input type="text" id="description" name="description"></input>
+        <label for="price">Price</label>
+        <input type="text" id="price" name="price"></input>
+        <label for="image_url">Image URL</label>
+        <input type="url" id="homepage" name="homepage"></input>
+      </form>
+    </div>
+  );
+}
 
 function ProductsIndex() {}
 
